0a918db39c8a4c5b94560dacf9efa78e7ff29cc7
Alec Foster <sirslush@email.arizona.edu>
2018-12-02 16:14:17 -0700

    Add files via upload
    test commit

---
+++ src/ifStatement.hpp
@@ -0,0 +1,34 @@
+//
+//  ifStatement.hpp
+//  hlysn
+//
+//  Created by Alec Foster on Sunday12/2.
+//
+
+#ifndef ifStatement_hpp
+#define ifStatement_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <istream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <string>
+
+class ifStatement {
+private:
+    std::string condition;
+    bool elseStatement = false;
+    std::string inIf;
+    std::string inElse;
+public:
+    ifStatement() {};
+    ifStatement(std::string word, std::string inIfState) : condition(word), inIf(inIfState) {};
+    ifStatement(std::string word,std::string inIfState, bool elsestate, std::string inElseState) : condition(word), inIf(inIfState), elseStatement(elsestate), inElse(inIfState) {};
+};
+
+//test
+#endif /* ifStatement_hpp */
